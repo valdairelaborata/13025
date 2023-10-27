2265f72a1a427e9b8ea75c5c553215b698aad215
valdairelaborta <valdairelaborata@gmail.com>
2023-10-27 20:41:14 -0300

    fundamentos

--- heranca.py
+++ heranca.py
@@ -4,10 +4,25 @@ class Carro():
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
+        self.__velocidade = 0
 
     def descricao(self):
         return f" Ano:{self.ano} marca:{self.marca} modelo:{self.modelo}"
     
+
+    def acelerar(self, valor):
+        self.__velocidade += valor
+
+
+    @property
+    def velocidade(self):
+        return self.__velocidade
+    
+    
+    @velocidade.setter
+    def velocidade(self, valor):
+        self.__velocidade = valor
+
 
 
 class CarroEsportivo(Carro):
@@ -20,12 +35,36 @@ class CarroEsportivo(Carro):
         return super().descricao() + f" e tem velocidade máxima de {self.velocidadeMaxima}"
       
 
-   
+
+  
+class CarroSedan(Carro):
+
+    def __init__(self, marca, modelo, ano, tamanhoPortaMalas):
+        super().__init__(marca, modelo, ano)
+        self.tamanhoPortaMalas = tamanhoPortaMalas
+
+    def descricao(self):
+        return super().descricao() + f" e tem capacidade de {self.tamanhoPortaMalas} litros"
+      
 
 
 carro1 = CarroEsportivo("Honda", "Civic", "2003", "240")
-print(carro1.descricao())
 
-carro2 = CarroEsportivo("asd", "asd", "asd", "44")
+carro1.acelerar(20)
+carro1.acelerar(120)
+
+carro1.velocidade = 250
+
+print(carro1.velocidade)
+
+# print(carro1.obterVelocidade())
+
+
+# print(carro1.descricao())
+
+carro2 = CarroSedan("VW", "Voyage", 2020, 380)
+carro2.acelerar(20)
+
+# print(carro2.descricao())
 
 print("Fim")
