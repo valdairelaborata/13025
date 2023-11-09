22f8aef784115d5187895f668d5724e072d1799d
Valdair <valdairelaborata@gmail.com>
2023-11-09 20:29:17 -0300

    lab

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import requests
 
 
-response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+response = requests.get("https://viacep.com.br/ws/81710230/json/")
 
 if response.status_code == 200:
     data = response.json()
-
-    print(data["title"])
-    print(data["body"])
+    print(f'Olá eu moro na rua {data["logradouro"]}, no bairro {data["bairro"]}')
    
 
 else:
     print("Erro ao tentar fazer o get")
-    
+
 
